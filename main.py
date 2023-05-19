9d0cec3b8b32abc59828e6ce2936b9a1544effd3
JaMinFu <408996194@qq.com>
2023-05-19 23:39:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -215,11 +215,13 @@ data = {
     "color": get_random_color()
   },
   "daily_eng":{
-     "value": day_en['newslist'][0]['content'],
+     # "value": day_en['newslist'][0]['content'],
+    "value": 'Today is a special day,let it be known throughout the land that my love for thee shall forever stand.',
     "color": get_random_color()
   },
   "day_zh":{
-   "value": day_en['newslist'][0]['note'],
+   # "value": day_en['newslist'][0]['note'],
+    "value": '让这份情传遍神州，我对你的爱永不凋零。',
     "color": get_random_color()
   },
   "sum_days":{
